bebb283e92259cb6f7dac1c770e0dba83a01b111
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-13 23:08:09 +0900

    3/12

--- seimon100/ex02/main.cpp
+++ seimon100/ex02/main.cpp
@@ -19,8 +19,32 @@ typedef vector<PII> VP;
 #define SORT(V) sort((V).begin(), (V).end())
 #define RSORT(V) sort((V).rbegin(), (V).rend())
 
+bool is_char_acgt(char c)
+{
+    return (c == 'A' || c == 'C' || c == 'G' || c == 'T');
+}
+
 int main()
 {
-    int n;
-    cin >> n;
+    string s;
+    cin >> s;
+
+    int tmp_size = 0;
+    int max_size = 0;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (is_char_acgt(s.at(i)))
+        {
+            tmp_size++;
+            if (max_size < tmp_size)
+                max_size = tmp_size;
+        }
+        else
+        {
+            if (max_size < tmp_size)
+                max_size = tmp_size;
+            tmp_size = 0;
+        }
+    }
+    cout << max_size << endl;
 }
