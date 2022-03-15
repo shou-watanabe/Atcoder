43a497e5c63f60d6292422a3f4ba75b58ead66ec
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-15 21:30:14 +0900

    WIP

---
+++ seimon100/ex03/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef vector<PII> VP;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+
+#define pb push_back
+#define paired make_pair
+#define ALL(a) (a).begin(), (a).end()
+#define PRINT(V)       \
+    for (auto v : (V)) \
+    cout << v << ' '
+#define SORT(V) sort((V).begin(), (V).end())
+#define RSORT(V) sort((V).rbegin(), (V).rend())
+
+signed main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    REP(i, m - 1)
+    {
+        FOR(j, i + 1, m)
+        {
+            //
+        }
+    }
+}
---
+++ seimon100/ex05/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef vector<PII> VP;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+
+#define pb push_back
+#define paired make_pair
+#define ALL(a) (a).begin(), (a).end()
+#define PRINT(V)       \
+    for (auto v : (V)) \
+    cout << v << ' '
+#define SORT(V) sort((V).begin(), (V).end())
+#define RSORT(V) sort((V).rbegin(), (V).rend())
+
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+signed main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+}
